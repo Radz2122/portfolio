7beda8accc5ceda4893559d1ccb88f49df4e07bb
Radz2122 <70171361+Radz2122@users.noreply.github.com>
2021-03-05 21:44:35 -0500

    FIN INFO SLIDE IN

--- src/Projet.jsx
+++ src/Projet.jsx
@@ -2,9 +2,24 @@ import React from 'react';
 import './Projet.scss';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 import Tooltip from '@material-ui/core/Tooltip';
+import { makeStyles } from '@material-ui/core/styles';
+import Switch from '@material-ui/core/Switch';
+import Paper from '@material-ui/core/Paper';
+import Collapse from '@material-ui/core/Collapse';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: 350,
+  },
+  container: {
+    display:'block',
+  },
+
+}));
 
 
 export default function Projet(props) {
+  //toooltip  handler
     const [open, setOpen] = React.useState(false);
 
     const handleClose = () => {
@@ -18,15 +33,28 @@ export default function Projet(props) {
     function infoAdd(){
         window.open(props.url, "_blank");
     }
-    return (
+    //sliding in for info
+    const classes = useStyles();
+    const [checked, setChecked] = React.useState(false);
+
+    const handleChange = () => {
+      setChecked((prev) => !prev);
+};
+  return (
     <div className="Projet">
-      <button className="ouvreInfo">+</button>
-      <img  className="imgProjet"src={`images-projets/${props.id}.png`} alt={props.titre}/>
-        <div className="titreProjet">{props.titre}</div>
-        <p className="descProjet">{props.desc}</p>
-        <Tooltip arrow disableFocusListener disableTouchListener open={open} onClose={handleClose} onOpen={handleOpen} title="Voir plus" placement="left">
-            <button className="plusInfo"><NavigateNextIcon onClick={infoAdd}/></button>
-        </Tooltip>   
+      <div className={classes.container}>
+        <button className="ouvreInfo" onClick={handleChange}>+</button>
+        <Collapse  in={checked}>
+          <div className="titreProjet">{props.titre}</div>
+          <p className="descProjet">{props.desc}</p>
+            <div className="plusInfoContainer">
+              <Tooltip  arrow disableFocusListener disableTouchListener open={open} onClose={handleClose} onOpen={handleOpen} title="Voir plus" placement="left">
+                <button className="plusInfo"><NavigateNextIcon style={{ fontSize:30}} onClick={infoAdd}/></button>
+              </Tooltip> 
+            </div>
+        </Collapse>
+      </div>
+      <img  className="imgProjet"src={`images-projets/${props.id}.png`} alt={props.titre}/>  
     </div>
     );
   }
